9295a3e20404c39d08d5bf6d476e41adbec02c6a
NiumXp <43611165+NiumXp@users.noreply.github.com>
2020-07-26 22:01:53 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="async-nekos.life-wrapper",
+      author="NiumXp",
+      url="https://github.com/NiumXp/Async-nekos.life-wrapper",
+      version="0.0.1",
+      packages=["anekos"],
+      license="MIT",
+      description="An unoficial async wrapper for nekos.life API!",
+      python_requires=">=3.8"
+)
