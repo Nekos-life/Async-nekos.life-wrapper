f745d04bd91804c3fe6de74486f13ccba9807dde
NiumXp <43611165+NiumXp@users.noreply.github.com>
2020-08-06 18:33:13 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(name="async-nekos.life-wrapper",
       version="0.0.1",
       packages=["anekos"],
       license="MIT",
-      description="An unoficial async wrapper for nekos.life API!",
+      description="An unofficial async wrapper for nekos.life API!",
       python_requires=">=3.8"
 )
