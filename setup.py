b43bfc3e1afe57322f53165e13706183fa6c8f9b
NiumXp <formigacomcaimbra@gmail.com>
2022-07-18 14:48:10 -0300

    change name of the project in setup.py to upload to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-setup(name="async-nekos.life-wrapper",
-      author="NiumXp",
-      url="https://github.com/Nekos-life/Async-nekos.life-wrapper",
-      version="1.0.0",
-      packages=["anekos"],
-      install_requires=["aiohttp>=3.6.2"],
-      license="MIT",
-      description="An unofficial async wrapper for nekos.life API!",
-      python_requires=">=3.6"
+setup(
+    name="anekos",
+    author="NiumXp",
+    url="https://github.com/Nekos-life/Async-nekos.life-wrapper",
+    version="1.0.0",
+    packages=["anekos"],
+    install_requires=["aiohttp>=3.6.2"],
+    license="MIT",
+    description="An unofficial async wrapper for nekos.life API!",
+    python_requires=">=3.6"
 )
