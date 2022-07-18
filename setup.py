a2249a8c7f8a7e3bf800fc0e42cfffc76bb6efc3
igna <igna@cocaine.ninja>
2022-07-18 15:25:16 -0400

    add auto version checking(?) and revert to "

--- setup.py
+++ setup.py
@@ -1,40 +1,46 @@
 from setuptools import setup
+from re import search, MULTILINE
 
 requirements = []
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-readme = ''
-with open('README.rst') as f:
+readme = ""
+with open("README.rst") as f:
     readme = f.read()
 
+version = ''
+with open('anekos/__init__.py') as f:
+    version = search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), MULTILINE).group(1)
+
+
 setup(
-    name='anekos',
-    author='NiumXP',
+    name="anekos",
+    author="NiumXP",
     project_urls={
-        'Website': 'https://nekos.life',
-        'Issue tracker': 'https://github.com/Nekos-life/Async-nekos.life-wrapper',
+        "Website": "https://nekos.life",
+        "Issue tracker": "https://github.com/Nekos-life/Async-nekos.life-wrapper",
     },
-    version='1.0.6',
-    packages=['anekos'],
-    license='MIT',
-    description='An unofficial asynchronous wrapper for nekos.life API',
+    version=version,
+    packages=["anekos"],
+    license="MIT",
+    description="An unofficial asynchronous wrapper for nekos.life API",
     long_description=readme,
-    long_description_content_type='text/restructured',
+    long_description_content_type="text/restructured",
     include_package_data=True,
     install_requires=requirements,
-    python_requires='>=3.8.0',
+    python_requires=">=3.8.0",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
     ],
 )
